dbedf63598d86ff4a38eebdea48f6f8e791126fe
ismail berriss <ismailberriss@gmail.com>
2023-04-24 02:26:32 +0100

    Adding the post/reply forum view

---
+++ resources/views/ForumPost.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forum - Bases de Données</title>
+    <!-- Website favicon-->
+    <link rel="shortcut icon" href="../img/fsa_agadir.png" type="image/x-icon">
+    <!-- Bootstrap 05 -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <!-- Main CSS File -->
+    <link rel="stylesheet" href="../css/ForumPost.css" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../css/all.min.css" />
+    <!-- Google Fonts - Open Sans -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- Start Header -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../img/fsa_agadir.png" alt="" width="40" height="30" class="d-inline-block align-text-top">
+                FSA-Online
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            Tableau de bord
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Formations
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            A propos
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Contactez-nous
+                        </a>
+                    </li>
+                </ul>
+                <div class="dropdown" >
+                    <button class="btn dropdown-toggle" type="button" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="../img/fsa_agadir.png" alt="" width="40" height="30" >
+                        Ismail Berriss
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="Profile.php">
+                                <i class="fa-solid fa-user"></i>
+                                Profile
+                            </a>
+                        </li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="fa-solid fa-power-off fa-lg"></i>
+                                Déconnexion
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="reply">
+        <div class="container">
+            <div class="post">
+                <div class="box">
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+                    <div class="content">
+                        <img src="../img/professeur.jpg" alt="">
+                        <div class="content-text">
+                            <span>Par Ismail Berriss</span>
+                            <span>Il ya 1 heure dans Bases de Données</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="add-container">
+                <a class="add-btn" type="button" href="#reply-form">
+                    <span>
+                        <i class="fa-solid fa-pen"></i>
+                    </span>
+                    Répondre
+                </a>
+            </div>
+            <div class="replies">
+                <div class="box">
+                    <div class="img-container">
+                        <img src="../img/professeur.jpg" alt="">
+                        <span>Ismail Berriss</span>
+                        <span>Etudiant</span>
+                    </div>
+                    <div class="content">
+                        <span>Publié il y a 1 heure</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dignissimos, dolorem
+                            doloremque eos exercitationem facilis maiores minus mollitia nostrum nulla, optio
+                            possimus sapiente similique ullam ut vel voluptate voluptatem voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium illo laborum nulla quae! Asperiores blanditiis ea fugit, id illum, modi molestiae quaerat quidem repellat repellendus repudiandae similique sunt voluptate voluptatibus?</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="img-container">
+                        <img src="../img/professeur.jpg" alt="">
+                        <span>Ismail Berriss</span>
+                        <span>Professeur</span>
+                    </div>
+                    <div class="content">
+                        <span>Publié il y a 1 heure</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dignissimos, dolorem doloremque eos exercitationem facilis maiores minus mollitia nostrum nulla, optio possimus sapiente similique ullam ut vel voluptate voluptatem voluptatibus.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="img-container">
+                        <img src="../img/professeur.jpg" alt="">
+                        <span>Ismail Berriss</span>
+                        <span>Etudiant</span>
+                    </div>
+                    <div class="content">
+                        <span>Publié il y a 1 heure</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dignissimos, dolorem doloremque eos exercitationem facilis maiores minus mollitia nostrum nulla, optio possimus sapiente similique ullam ut vel voluptate voluptatem voluptatibus.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="img-container">
+                        <img src="../img/professeur.jpg" alt="">
+                        <span>Ismail Berriss</span>
+                        <span>Etudiant</span>
+                    </div>
+                    <div class="content">
+                        <span>Publié il y a 1 heure</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dignissimos, dolorem doloremque eos exercitationem facilis maiores minus mollitia nostrum nulla, optio possimus sapiente similique ullam ut vel voluptate voluptatem voluptatibus.</p>
+                    </div>
+                </div>
+                <div class="reply-form" id="reply-form">
+                    <div class="image">
+                        <img src="../img/professeur.jpg" alt="">
+                    </div>
+                    <form class="needs-validation" action="" method="post">
+                        <div>
+                            <textarea class="form-control" name="reply" rows="7"
+                                      required style="resize: none"></textarea>
+                            <div class="valid-feedback">
+                                C'est bon!
+                            </div>
+                            <div class="invalid-feedback">
+                                Veuillez insérer du texte.
+                            </div>
+                        </div>
+                        <div class="submit">
+                            <button class="add-btn" type="button">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg>
+                                    <input type="submit" value="Ajouter répondre">
+                                </span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/all.min.js"></script>
+    <script>
+        (function () {
+            'use strict'
+
+            var forms = document.querySelectorAll('.needs-validation')
+
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })()
+    </script>
+</body>
+</html>
